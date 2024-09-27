4762e821f83539f0d9655eb200869d0234d45b70
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-09-26 23:13:17 -0400

    Use Rishabh's completion backend.

--- index.js
+++ index.js
@@ -3,30 +3,34 @@
 (async function(codioIDE, window) {
   
   // Refer to Anthropic's guide on system prompts here: https://docs.anthropic.com/claude/docs/system-prompts
-  const systemPrompt = "System Prompt for the LLM goes here"
-  
+  const systemPrompt = "Copy what the user says exactly"
+
   // register(id: unique button id, name: name of button visible in Coach, function: function to call when button is clicked) 
-  codioIDE.coachBot.register("iNeedHelpButton", "I have 17 questions", onButtonPress)
+  codioIDE.coachBot.register("iNeedHelpButton", "What is project 1 about?", onButtonPress)
 
   // function called when I have a question button is pressed
   async function onButtonPress() {
-
     // Function that automatically collects all available context 
     // returns the following object: {guidesPage, assignmentData, files, error}
-    const context = await codioIDE.coachBot.getContext()
-
+    // const context = await codioIDE.coachBot.getContext()
     // the messages object that will contain the user prompt and/or any assistant responses to be sent to the LLM
     // Refer to Anthropic's guide on the messages API here: https://docs.anthropic.com/en/api/messages
     let messages = []
-
-    const userPrompt = "User prompt for the LLM goes here - can contain context from the object returned by getContext()"
-
+    const response = await fetch("http://0.0.0.0:5005/ask", {
+      method: "POST",
+      body: JSON.stringify({
+        "course_id": "506795",
+        "message": "What is project 1 about?"
+      }),
+    })
+    const json = await response.json()
+    const msg = json.message
+    // const userPrompt = "User prompt for the LLM goes here - can contain context from the object returned by getContext()"
     // Add user prompt to messages object
     messages.push({
         "role": "user", 
-        "content": userPrompt
+        "content": msg
     })
-
     // Send the API request to the LLM with all prompts and context 
     const result = await codioIDE.coachBot.ask({
       systemPrompt: systemPrompt,
@@ -36,8 +40,3 @@
   }
 // calling the function immediately by passing the required variables
 })(window.codioIDE, window)
-
- 
-
-  
-  
