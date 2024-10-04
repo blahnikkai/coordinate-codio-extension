16445195d338c3ed3a038b908416b01258872027
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-10-03 23:20:09 -0400

    Create multi-turn frontend (user prompted for typed input).

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
-// Wrapping the whole extension in a JS function 
-// (ensures all global variables set in this extension cannot be referenced outside its scope)
-(async function(codioIDE, window) {
+async function getResponse(question) {
+  return await fetch("http://0.0.0.0:5005/ask", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "course_id": "506795",
+      "message": question,
+    }),
+  })
+}
+
+async function main(codioIDE, window) {
   
-  // Refer to Anthropic's guide on system prompts here: https://docs.anthropic.com/claude/docs/system-prompts
-  const systemPrompt = "Copy what the user says exactly"
+  codioIDE.coachBot.register("QuestionButton", "I have a question", onButtonPress)
 
-  // register(id: unique button id, name: name of button visible in Coach, function: function to call when button is clicked) 
-  str1 = "What is project 1 about?"
-  str2 = "What is the most recent announcement?"
-  codioIDE.coachBot.register("button1", str1, onButtonPress.bind(str1))
-  codioIDE.coachBot.register("button2", str2, onButtonPress.bind(str2))
+  const systemPrompt = "Help the user with their questions. The user can type 'Quit' to quit"
 
-  // function called when I have a question button is pressed
-  async function onButtonPress(question) {
-    // Function that automatically collects all available context 
-    // returns the following object: {guidesPage, assignmentData, files, error}
-    // const context = await codioIDE.coachBot.getContext()
-    // the messages object that will contain the user prompt and/or any assistant responses to be sent to the LLM
-    // Refer to Anthropic's guide on the messages API here: https://docs.anthropic.com/en/api/messages
-    let messages = []
-    const response = await fetch("http://0.0.0.0:5005/ask", {
-      method: "POST",
-      body: JSON.stringify({
-        "course_id": "506795",
-        "message": question,
-      }),
-    })
-    const json = await response.json()
-    const msg = json.message
-    // const userPrompt = "User prompt for the LLM goes here - can contain context from the object returned by getContext()"
-    // Add user prompt to messages object
-    messages.push({
-        "role": "user", 
-        "content": msg
-    })
-    // Send the API request to the LLM with all prompts and context 
-    const result = await codioIDE.coachBot.ask({
-      systemPrompt: systemPrompt,
-      messages: messages
-    })
+  async function onButtonPress() {
     
+    let messages = []
+    
+    codioIDE.coachBot.write("Ask your question in the text input below, or type 'Quit' to quit")
+    while(true) {
+      
+      // const response = await getResponse(str1)
+      // const json = await response.json()
+      // const msg = json.message
+      
+      const user_input = await codioIDE.coachBot.input()
+      
+      if(user_input == "Quit") {
+        break;
+      }
+      
+      messages.push({
+          "role": "user", 
+          "content": user_input,
+      })
+      
+      const llm_response = await codioIDE.coachBot.ask(
+        {
+          systemPrompt: systemPrompt,
+          messages: messages,
+        }, 
+        {
+          preventMenu: true
+        },
+      )
+
+      messages.push({
+        "role": "assistant",
+        "content": llm_response.result,
+      })
+
+      if (messages.length >= 10) {
+        const removedElements = messages.splice(0, 2)
+      }
+
+    }
+    codioIDE.coachBot.write("Feel free to ask some more questions whenever you want to know something!")
+    codioIDE.coachBot.showMenu()
   }
-// calling the function immediately by passing the required variables
-})(window.codioIDE, window)
+}
+
+main(window.codioIDE, window)
+
+// async function testGetResponse() {
+//   const response = await getResponse("What's for breakfast?")
+//   const json = await response.json()
+//   console.log(json)
+//   const msg = json.message
+//   console.log(msg)
+// }
+
+// testGetResponse()
